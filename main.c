eb04dfae7e4c2f57d684c33eb6b5ff2ff77b6e0c
Bertrand <bertrand@debian-LenovoPro>
2014-08-19 12:36:07 +0200

    added instructions

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/** FizzBuzz Assignment :
+ * Write a program that prints the numbers from 1 to 100. But for multiples of
+ * three print "Fizz" instead of the number and for the multiples of five print
+ * "Buzz". For numbers which are multiples of both three and five print "FizzBuzz"
+ */
+
 int main(int argc, char* argv[]){
         if(!(argc % 3)) printf("Fizz");
         if(!(argc % 5)) printf("Buzz");
