5617fb9f39756a6f67158763c2758954b99ebd39
Bertrand <bertrand@debian-LenovoPro>
2014-08-19 12:30:46 +0200

    now in caps

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]){
-        if(!(argc % 3)) printf("fizz");
-        if(!(argc % 5)) printf("buzz");
+        if(!(argc % 3)) printf("Fizz");
+        if(!(argc % 5)) printf("Buzz");
         if(argc % 3 && argc % 5) printf("%i",argc);
         printf("\n");
         return argc == 100 ? 0 : main(++argc,0);
